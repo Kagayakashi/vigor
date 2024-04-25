25ab4ee003916c2f3895caeb95deccd9807de78e
Vyacheslav Odinokov <vyacheslav.odinokov@outlook.com>
2024-04-25 22:42:07 +0300

    feat: added events on home page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,6 +65,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title_en, :title_ru, :content_en, :content_ru)
+      params.require(:event).permit(:title_en, :title_ru, :content_en, :content_ru, :datetime)
     end
 end
